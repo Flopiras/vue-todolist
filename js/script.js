31d6548a261165d02ba1467fe6667cdecd835644
Flopiras <Flopiras@yahoo.it>
2023-06-20 15:22:45 +0200

    add tasks

--- js/script.js
+++ js/script.js
@@ -3,6 +3,33 @@ console.log('Vue ok', Vue);
 const app = Vue.createApp({
     data() {
         return{
+            tasks: [
+                {
+                    text: 'Conquistare il mondo',
+                    done: false
+                },
+                {
+                    text: 'Fare il pieno alla Morte Nera',
+                    done: false
+                },
+                {
+                    text: 'Cercare nuove reclute',
+                    done: false
+                },
+                {
+                    text: 'Intervista con Maurizio Costanzo',
+                    done: false
+                },
+                {
+                    text: 'Dare da mangiare agli unicorni',
+                    done: false
+                },
+                {
+                    text: 'Raccogliere margheritine',
+                    done: false
+                },
+                
+            ]
 
         }
     }
